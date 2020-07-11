d92ecf63cd05ad674d3ccf828f31bcfd1a847263
ANAΓKH <ma674359954@icloud.com>
2020-07-11 18:16:45 +0800

    Update SignTransaction.js

--- src/SignTransaction.js
+++ src/SignTransaction.js
@@ -1,13 +1,22 @@
 import React from 'react';
 const bsv = require('bsv');
 
-function scriptPubKeyCalculator (wif){
-    let bufwif = bsv.toBuffer(wif);
-    let privateKey = bsv.PrivateKey.fromWIF(bufwif);
-    let publicKey = bsv.PublicKey.fromPrivateKey(privateKey);
-    let scriptSha256PubKey = bsv.crypto.Hash.sha256(publicKey);
+function scriptPubKeyCalculator (address){
+    let decodeAddress = bsv.encoding.Base58Check.decode(address);
+    let scriptPubKey = decodeAddress.slice(1);
+    return scriptPubKey;
+//    let wif = 'Kyvu96nXzQmPT39G71AS9e8h8qia2dofp9wchSYwt6hUULzjBBdp';
+//    let privateKey = bsv.PrivateKey.fromWIF(wif);
+//    let publicKey = bsv.PublicKey.fromPrivateKey(privateKey);
+//    let pbID = 
+//    console.log('zaizheli');
+//    console.log(publicKey);
+//    console.log('zaizheli');
+//    return true;
+//    let bufpublicKey = bsv.encoding.BufferWriter.prototype.toBuffer(publicKey);
+//    let scriptSha256PubKey = bsv.crypto.Hash.sha256(bufpublicKey);
 //    let scriptPubKey = bsv.crypto.Hash.ripemd160(bsv.crypto.Hash.sha256(publicKey));
-    return scriptSha256PubKey;
+//    return scriptSha256PubKey;
 }
 
 class SignTransaction extends React.Component{
@@ -16,8 +25,7 @@ class SignTransaction extends React.Component{
         this.state = {};
     }
     render (){
-        let test2 = 'Kyvu96nXzQmPT39G71AS9e8h8qia2dofp9wchSYwt6hUULzjBBdp';
-        let test3 = scriptPubKeyCalculator(test2);
+        let test3 = scriptPubKeyCalculator('1D7nNuyHjsYtG1Pbk3stvcs6inqWUvCsZy');
         console.log(test3);
         return(
         <div></div>
