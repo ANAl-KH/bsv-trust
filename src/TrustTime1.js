16f99d5d257734a7eb3b8008fb8417e21a617ae8
ANAΓKH <ma674359954@icloud.com>
2020-07-25 11:31:55 +0800

    加中文注释

--- src/TrustTime1.js
+++ src/TrustTime1.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {DatePicker} from 'antd';
 import moment from 'moment';
+//bsv库，主要用到的库，用来构建交易
 const bsv = require('bsv');
 
 class TrustTime1 extends React.Component{
@@ -17,20 +18,24 @@ class TrustTime1 extends React.Component{
     handleTimeonChange(value,dateString){}
 
     handleTimeonOk(value){
+        //获取用户设定的信托到期时间，转换成unix时间戳的格式
         let unixTime = moment(value._d).unix();
         this.setState({time:unixTime});
-        console.log(this.state.time);
+        //console.log(this.state.time);
     }
 
     disableDate(current){
+        //信托的到期时间只能是未来，因此禁止时间选择框选择今天之前的日期
         return current < moment().startOf('day');
     }
 
     handleAddronChange(e){
+        //获取用户输入的信托目标地址
         this.setState({targetAddress: e.target.value});
     }
 
     async handleSetClick(){
+        //开始创建信托，检查用户是否已设定日期和目标地址
         if(this.state.time !== '' && this.state.targetAddress !== ''){
             try{
                 var correctTargetAddress = bsv.Address.fromString(this.state.targetAddress)
@@ -38,12 +43,16 @@ class TrustTime1 extends React.Component{
                 console.log(this.props.utxo.length)
                 console.log(this.props.totalBsvSat)
                 console.log(74*this.props.utxo.length)
+                //用户总余额减去所有手续费。手续费随用户地址里的UTXO数量变化。
                 var reduceSat = this.props.totalBsvSat - 400300 - 74*this.props.utxo.length;
                 console.log(reduceSat);
                 try{
+                    //创建第一笔交易，因为第二笔交易的SigHash Single只能对单个UTXO使用，所以需要将用户所有的UTXO集中起来。
+                    //第一个输出是给我们的手续费，第二个输出是用来构建第三笔交易的，第三个输出就是给第二笔交易用的单个UTXO
                     var reduceTx = bsv.Transaction().from(this.props.utxo).to('1KXZ29ssLh83hZcuzHAADXso37tUYt3Saw',400000).to(trustAddress,230).to(trustAddress,reduceSat).sign(this.props.privateKey);
                     var rawReduceTx = reduceTx.toBuffer().toString('hex');
                     console.log(rawReduceTx);
+                    //构建第二笔交易需要第一笔交易产生的第三个UTXO，没找到bsv库里有相关方法，只能手动构建UTXO
                     var reduceTxid = bsv.crypto.Hash.sha256sha256(reduceTx.toBuffer()).reverse().toString('hex');
                     var trustUtxo = {
                         'address':trustAddress,
@@ -53,9 +62,11 @@ class TrustTime1 extends React.Component{
                         'satoshis':reduceSat
                     };
                     console.log(trustUtxo);
+                    //使用第一笔交易的第三个输出构建第二笔交易，也就是信托交易，设定了nLockTime,使用了SigHash Single
                     var trustTx = bsv.Transaction().from(trustUtxo).to(correctTargetAddress,reduceSat-900130).to(correctTargetAddress,900000).lockUntilDate(this.state.time).sign(this.props.privateKey,3);
                     var rawTrustTx = trustTx.toBuffer().toString('hex');
                     console.log(rawTrustTx);
+                    //构建第三笔交易需要第一笔交易产生的第二个UTXO，手动构建
                     var carrierUtxo = {
                         'address':trustAddress,
                         'txId':reduceTxid,
@@ -63,12 +74,12 @@ class TrustTime1 extends React.Component{
                         'scriptPubKey':this.props.scriptPubKey,
                         'satoshis':230
                     };
-                    //var trustSignBuf = Buffer.from('BSVTRUSTLONGDELAY');
-                    //var trustSignHex = trustSignBuf.toString('hex');
+                    //构建第三笔交易，opreturn中放上标记字符串和第二笔交易的原始数据
                     var trustSign = 'BSVTRUSTLONGDELAY';
                     var carrierTx = bsv.Transaction().from(carrierUtxo).addSafeData(trustSign+rawTrustTx).sign(this.props.privateKey);
                     var rawCarrierTx = carrierTx.toBuffer().toString('hex');
                     console.log(rawCarrierTx);
+                    //把第一笔交易和第三笔交易的原始数据通过https://metasv.com/#bc60c7b38b post出去，post成功后信托就创建成功了
                     /*
                     try{
                         var postReduceRes = await fetch('/postapi/',{
--- src/UtxoInput1.js
+++ src/UtxoInput1.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import TrustTime1 from './TrustTime1';
+//bsv库，主要用到的库，用来构建交易
 const bsv = require('bsv');
 
+//根据私钥计算出对应的地址
+//用户输入的是WIF格式私钥，Wallet Import Format，需要转换成privateKey格式才能使用
 function toAddress(wif){
     let privateKey = bsv.PrivateKey.fromWIF(wif);
     let address = bsv.Address.fromPrivateKey(privateKey);
     return address.toString();
 }
+//根据地址计算出对应的锁定脚本
 function toScriptPubKey(address){
     let pubKeyHash = bsv.encoding.Base58Check.decode(address).slice(1);
     let scriptPubKey = `76a914${pubKeyHash.toString('hex')}88ac`;
@@ -16,6 +20,7 @@ function toScriptPubKey(address){
 class UtxoInput1 extends React.Component{
     constructor(props){
         super(props);
+        //err用来显示导入私钥过程中可能出现的各种错误信息，success用来判断是否已经成功导入私钥
         this.state = {wif:'',err:'',success:false};
         this.handleWifChange = this.handleWifChange.bind(this);
         this.handleWifClick = this.handleWifClick.bind(this);
@@ -29,14 +34,19 @@ class UtxoInput1 extends React.Component{
         var wif = this.state.wif;
         this.setState({success:false});
         try{
+            //判断用户输入的wif私钥格式是否正确，有问题的话bsv库会抛出错误，这一步不是很严谨因为有的字符串也可以顺利运行，不过影响不大
             var privateKey = bsv.PrivateKey.fromWIF(wif);
             var scriptPubKey = toScriptPubKey(toAddress(wif));
             var address = toAddress(wif)+'/utxo';
             try{
+                //用对应的地址从https://metasv.com/#utxo-2 查询该地址里的未花费输出UTXO
                 var res = await fetch(`/api/${address}`);
                 var utxoObj = await res.json();
                 console.log(utxoObj);
+                //示例：{"code":200,"data":[{"id":4701050,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"3541514998f58a38443878d0904033f519cb26820e9d88ccb5b22a90c3f34d1b","outputIndex":0,"value":500000,"height":576239,"ancestors":0,"descendents":0},{"id":5376917,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"e798e31cecf8a73ecf66402e702d28a91f99be46c09bee73a2e04a2068f2451f","outputIndex":0,"value":9990,"height":576240,"ancestors":0,"descendents":0},{"id":10733687,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"67ea4d9c3e9560bb59f7a1bb1824cd2f1cf7910812767bb5269c88479fa9353e","outputIndex":0,"value":7000,"height":576236,"ancestors":0,"descendents":0},{"id":11399113,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"0715d5d2a78aaa362f00f9a50026c849ece22e18e24b6469e79b2ce1df970042","outputIndex":0,"value":666000,"height":576235,"ancestors":0,"descendents":0},{"id":15263250,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"28d2b9d85c4a43f45d3365071914ad6652609d87341a3f624877d016d8081d58","outputIndex":0,"value":15085,"height":576240,"ancestors":0,"descendents":0},{"id":20292332,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"166a3ab4254f7dbb0a9ea5750bdc19254fb8597b30a8c393b8a48d3f73939775","outputIndex":0,"value":666000,"height":576241,"ancestors":0,"descendents":0},{"id":26557378,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"3f7b87b1735e3e21e3e60e0b3a09b3b9350176bdb2972bb2967a28a1df60179a","outputIndex":1,"value":666,"height":576228,"ancestors":0,"descendents":0},{"id":27026103,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"d0afbd423a80eb7460e9ebb2fb095aebdb4daba6fd0110aee2e358397e62c49c","outputIndex":429,"value":1,"height":309740,"ancestors":0,"descendents":0},{"id":28058794,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"fceec75763a4925e8efa068fc2086f0231db841b5d4d47b2ec0a9df7ee87c6a2","outputIndex":0,"value":196461,"height":576237,"ancestors":0,"descendents":0},{"id":31537708,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"0a21079a06c523fed3a41f890c4e96bb148433c90bf42d3c12649846af20f5b6","outputIndex":0,"value":100000000,"height":576232,"ancestors":0,"descendents":0},{"id":32092180,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"4844d71699b05b738fe54a80cad9a570ece202f05f7540b809307319239239ba","outputIndex":0,"value":250000,"height":576235,"ancestors":0,"descendents":0},{"id":36525589,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"4a979b3cc0810492529153f6f5322e406391a730e6628c9d206129ef4dd116d4","outputIndex":0,"value":9333,"height":576232,"ancestors":0,"descendents":0},{"id":37882544,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"63d3e80a9b4a03808beb57d8e104762d3177a467f87479779f866e223da504dc","outputIndex":0,"value":15187,"height":576236,"ancestors":0,"descendents":0},{"id":38862855,"address":"12higDjoCCNXSA95xZMWUdPvXNmkAduhWv","txid":"ce15088a047265e120d59b8fdad71b12b4fd3a40fcb12fda474e5b2324c7bbe1","outputIndex":0,"value":555555,"height":576243,"ancestors":0,"descendents":0}],"success":true}
+                //判断是否查询成功
                 if (utxoObj.code === 200 && utxoObj.success === true){
+                    //判断余额是否足够
                     if(utxoObj.data!==[]){
                         var utxoData = utxoObj.data;
                         var totalBsvSat = parseInt(utxoData.reduce((totalSatoshis,item) => totalSatoshis + item.value,0));
@@ -44,8 +54,10 @@ class UtxoInput1 extends React.Component{
                         if(totalBsvSat > parseInt(1350000)){
                             if(parseInt(utxoData.reduce((totalAncestors,item) => totalAncestors + item.ancestors,0)) < parseInt(20)){
                                 utxoData.forEach(element => {
+                                    //根据bsv库的要求对UTXO进行改造，将value改名为satoshis，增加scriptPubKey
                                     element['satoshis']=element['value'];
                                     element['scriptPubKey']=scriptPubKey;
+                                    //将所需的信息都放到state中
                                     this.setState({err:`成功导入私钥，余额为${totalBsv}BSV`,success:true,utxo:utxoData,privateKey:privateKey,totalBsvSat:totalBsvSat,scriptPubKey:scriptPubKey});
                                 });
                             }else{this.setState({err:'未确认utxo链式调用超过25层，请稍后再创建信托'})}
@@ -60,6 +72,8 @@ class UtxoInput1 extends React.Component{
         const wif = this.state.wif;
         const err = this.state.err;
         const success = this.state.success;
+        //使用err向用户显示相关错误信息，每次出错都会调用setState更新错误信息
+        //直到私钥导入成功才渲染信托日期和信托目标地址设定组件，将所需的信息传递到组件props中
         return(
             <div>
                 <div>请输入单个地址的WIF格式的私钥:</div>
